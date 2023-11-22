2c0ff1085a0356ba0efb2acdbd2eac0927232a10
Joshua Lin <joshlin@mit.edu>
2023-11-21 19:50:31 -0800

    finv now returns None if it doesn't find a match

--- src/chropy/utils.py
+++ src/chropy/utils.py
@@ -5,14 +5,25 @@ General utility functions
 import re
 
 def finv(x,pattern):
-    """
-    Inverse f-string function. For example, an f-string might be
+    """Inverse f-string function. 
+
+    Parameters
+    ----------
+    x: The string to be matched to the pattern
+    pattern: A string that represents the pattern to search for. Should include substrings with groups encapsulated in curly braces, such as '{a}'. 
+
+    Returns
+    -------
+    A dictionary with keys given by the groups to search for. If the pattern could not be matched, returns None
+
+    Examples
+    --------
+    For example, an f-string might be
       f'_{a}_{b}_' = '_apple_banana_'
     To invert it, we can run
-
     >>> finv('_apple_banana_','_{a}_{b}_')
     {'a': 'apple', 'b': 'banana'}
-
+    
     finv will try and make the matched groups go as far to the right as possible
     >>> finv("yxxy","{a}x{b}")
     {'a': 'yx', 'b': 'y'}
@@ -24,13 +35,21 @@ def finv(x,pattern):
     Typical use-case
     >>> finv("prop_2+1f_24nt64_IWASAKI_b2.13_ls16_M1.8_ms0.04_mu0.005_mv0.005_srcx8y8z8t5.975","ms{ms}_mu{mu}_mv{mv}_srcx{x}y{y}z{z}t{t}.{cfg}")
     {'ms': '0.04', 'mu': '0.005', 'mv': '0.005', 'x': '8', 'y': '8', 'z': '8', 't': '5', 'cfg': '975'}
+
+    If the pattern can not be matched, returns None
+    >>> finv("apple_yummy_apple","banana_{x}_banana")
+    None
     """
     #Make sure that the special characters are escaped properly
     a0 = re.sub('\.','\\.',pattern)
     a1 = re.sub('\_','\\_',a0)
     a2 = re.sub('\{','(?P<',a1)
     a3 = re.sub('\}','>.*)',a2)
-    return re.search(a3,x).groupdict()
+
+    temp = re.search(a3,x)
+    if temp is None:
+        return None
+    return temp.groupdict()
     
 def split_list(l,n):
     """
